94a7554bd5e338d954be12a3c45319c5b2d4370b
Erkut Resuloğlu <erkutresul@gmail.com>
2026-07-15 15:23:29 +0300

    Change 'çevrimiçi' to 'Çevrimiçi' for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -572,7 +572,7 @@ export default function App() {
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 bg-emerald-400"></span>
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-emerald-500"></span>
                 </span>
-                <span className="text-white font-semibold">çevrimiçi</span>
+                <span className="text-white font-semibold">Çevrimiçi</span>
               </div>
 
               {/* Paylaş Butonu */}
